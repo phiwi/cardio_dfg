97cbcbf234efcf53414cd09206a3b54f0c0b11d4
Philipp Wiesenbach <philipp.wiesenbach@gmail.com>
2025-06-25 16:25:50 +0200

    Add slide numbering.

---
+++ global-bottom.vue
@@ -0,0 +1,15 @@
+      
+<template>
+  <!-- 
+    This footer will be added to every slide.
+    The z-50 class ensures it renders on top of any custom backgrounds.
+  -->
+  <footer 
+    v-if="$slidev.nav.currentPage > 1"
+    class="absolute bottom-4 right-6 text-sm text-gray-400 z-50"
+  >
+    {{ $slidev.nav.currentPage }}
+  </footer>
+</template>
+
+    
